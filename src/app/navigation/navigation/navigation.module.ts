0ff65dde3f1672340a5d40261875b8e564df3b2a
ethan-humphries <ethan.humphries1993@hotmail.com>
2020-02-22 16:37:50 +1100

    add routing

--- src/app/navigation/navigation/navigation.module.ts
+++ src/app/navigation/navigation/navigation.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './navigation.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from 'src/app/home/home/home.component';
 
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [NavigationComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   exports: [
     NavigationComponent
